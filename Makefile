23553ac0dc31be8a0598e265e7d26abddb0dbda2
Aleh Dzenisiuk <dzenisiuk@gmail.com>
2017-11-18 02:40:52 +0000

    Add the functionality of other battery-related srcripts

--- Makefile
+++ Makefile
@@ -5,6 +5,8 @@ pocketchip-batt: pocketchip-batt.c
 
 install: pocketchip-batt
 	cp $< /usr/sbin/pocketchip-batt
+	rm -rf /usr/lib/pocketchip-batt
+	ln -s /run/pocketchip-batt /usr/lib/pocketchip-batt
 
 .PHONY: install
 
--- pocketchip-batt.c
+++ pocketchip-batt.c
@@ -22,7 +22,6 @@ int i2c_device;
 
 static int _i2c_write(const uint8_t *buf, int len) {
 	if (write(i2c_device, buf, len) != len) {
-		perror("I2C write failed");
 		return 0;
 	}
 	return 1;
@@ -30,17 +29,15 @@ static int _i2c_write(const uint8_t *buf, int len) {
 
 static int _i2c_read_reg(uint8_t reg, uint8_t *buf, int len) {
 	if (write(i2c_device, &reg, 1) != 1) {
-		perror("Could not write register address");
 		return 0;
 	}
 	if (read(i2c_device, buf, len) != len) {
-		perror("I2C read failed");
 		return 0;
 	}
 	return 1;
 }
 
-int main() {
+static int read_battery(int *charging, int *voltage) {
 
 	if ((i2c_device = open("/dev/i2c-0", O_RDWR)) < 0) {
 		perror("Could not open the adapter device");
@@ -54,28 +51,58 @@ int main() {
 
 	// The comment in the original battery.sh for this was "force ADC enable for battery voltage and current".
 	if (!_i2c_write((const uint8_t[]){ 0x82, 0xC3 }, 2)) {
+		perror("Could not enable the ADC");
 		return 3;
 	}
 
-	// We are interested only in printing of the info actually used by pocketchip-batt script.
-	uint8_t charging;
-	float voltage;
-
 	uint8_t buf[8];
 	if (!_i2c_read_reg(0x01, buf, 1)) {
+		perror("Could not read charging status");
 		return 5;
 	}
-	charging = (buf[0] >> 6) & 1; 
+	*charging = (buf[0] >> 6) & 1; 
 	
 	if (!_i2c_read_reg(0x78, buf, 1)) {
+		perror("Could not read the voltage (MSB)");
 		return 6;
 	}
+	
 	if (!_i2c_read_reg(0x79, buf + 1, 1)) {
+		perror("Could not read the voltage (LSB)");
 		return 7;
 	}
-	voltage = (((uint16_t)buf[0] << 4) | (buf[1] & 0x0F)) * 1.1;
+	*voltage = (((uint16_t)buf[0] << 4) | (buf[1] & 0x0F)) * 1.1;
 
-	// Not printing these params anymore as instead of replacing the battery.sh I've decided to replace pocketchip-batt and extra output will cause more logging by systemd.
+	return 0;
+}
+
+static int has_fs_flag(const char *name) {
+	if (access(name, F_OK) == 0) {
+		return 1;
+	}
+	int f = open(name, O_CREAT | O_WRONLY);
+	if (f == 0) {
+		perror("Could not create a flag file");
+	} else {
+		close(f);
+	}
+	return 0;
+}
+
+static void remove_fs_flag(const char *name) {
+	unlink(name);
+}
+
+int main() {
+
+	int charging;
+	int voltage;
+	int result = read_battery(&charging, &voltage);
+	if (result)
+		return result;
+
+	// Not printing these params anymore as instead of replacing the battery.sh I've decided to replace 
+	// pocketchip-batt and extra output would just cause more logging by systemd.
 	/*~
 	// Let's print to stdout just in case this script is used instead of battery.sh .
 	printf("CHARG_IND=%d\n", charging);
@@ -85,6 +112,10 @@ int main() {
 	//
  	// And this is what the pocket-batt actually does (except for averaging).
 	//
+
+	// We want the directory /usr/lib/pocketchip-batt to actually be on a temporary FS,
+	// so making a directory in /run and there will be a symlink pointing there created via a makefile.
+	mkdir("/run/pocketchip-batt", 0777);
 
 	FILE *charging_file = fopen("/usr/lib/pocketchip-batt/charging", "w");
 	if (charging_file == NULL) {
@@ -99,14 +130,44 @@ int main() {
 	if (voltage_file == NULL) {
 		perror("Could not update the voltage file");
 	} else {
-		fprintf(voltage_file, "%.0f\n", voltage);
+		fprintf(voltage_file, "%d\n", voltage);
 		fclose(voltage_file);
 	}
 
-	close(i2c_device);
+	//
+	// And it looks like it is not hard to do what the rest of the scripts were doing
+	//
+
+	// I am not sure who else is using these flag files, otherwise would put them to /run as well.
+	const char warn05_flag[] = "/home/chip/.pocketchip-batt/warn05";
+	const char warn15_flag[] = "/home/chip/.pocketchip-batt/warn15";
+	if (!charging) {
+		mkdir("/home/chip/.pocketchip-batt", 0777);	
+		if (voltage < 3300) {
+			// pocketchip-off00
+			system("DISPLAY=:0 pocket-off");
+		} else if (voltage < 3375) {
+			// pocketchip-warn05
+			if (!has_fs_flag(warn05_flag)) {
+				system("DISPLAY=:0 pocket-exit-5");
+			}
+		} else if (voltage < 3500) {
+			// pocketchip-warn15
+			if (!has_fs_flag(warn15_flag)) {
+				system("DISPLAY=:0 pocket-exit-15");
+			}
+		}
+	}
+	// pocketchip-warn05
+	if (charging || voltage > 3400) {
+		remove_fs_flag(warn05_flag);
+	}
+	// pocketchip-warn15
+	if (charging || voltage > 3550) {
+		remove_fs_flag(warn15_flag);
+	}
 
 	return 0;
 }
-
 
 // vim: ts=4:sw=4
